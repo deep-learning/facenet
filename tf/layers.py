5753cb46e8fe37d82683f395f4f278237b9b6a70
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-12-15 14:55:34 +0800

    eanble variable sharing

--- tf/layers.py
+++ tf/layers.py
@@ -11,18 +11,24 @@ def layer(input, weight_shape, bias_shape):
 def simple_network(input):
     with tf.variable_scope('layer_1'):
         output_1 = layer(input, [784, 100], [100])
+        print(output_1.name)
 
     with tf.variable_scope('layer_2'):
         output_2 = layer(output_1, [100, 50], [50])
+        print(output_2.name)
 
     with tf.variable_scope('layer_3'):
         output_3 = layer(output_2, [50, 10], [10])
+        print(output_3.name)
 
     return output_3
 
+# name_scopesymotion-prefix)w 是给op_name加前缀, variable_scope是给get_variable()创建的变量的名字加前缀。
+# todo ???
 if __name__ == '__main__':
-    i_1 = tf.placeholder(tf.float32, [1000, 784], name='i_1')
-    simple_network(i_1)
-    # following should fail as reuse is by default disabled
-    # i_2 = tf.placeholder(tf.float32, [1000,784], name='i_2')
-    # simple_network(i_2)
+    with tf.variable_scope("shared_variables") as scope:
+        i_1 = tf.placeholder(tf.float32, [1000, 784], name='i_1')
+        simple_network(i_1)
+        scope.reuse_variables()
+        i_2 = tf.placeholder(tf.float32, [1000,784], name='i_2')
+        simple_network(i_2)
