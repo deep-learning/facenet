5876facd0ac4158b670ed9e1ae31b91e09273007
Zhenglai Zhang <zhenlaizhang@hotmail.com>
2017-12-17 16:10:49 +0800

    check pip lib installation

--- tf/000_setup.py
+++ tf/000_setup.py
@@ -1,10 +1,4 @@
-import cv2
-import tensorflow as tf
-import numpy as np
-import pandas as pd
+libs = ('cv2', 'tensorflow', 'numpy', 'pandas', 'keras')
 
-
-print(cv2.__version__)
-print(tf.__version__)
-print(np.__version__)
-print(pd.__version__)
+for lib in libs:
+    exec('import {}; print("{}: ", {}.__version__)'.format(lib, lib, lib))
