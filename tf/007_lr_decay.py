840397ba031d936edaa8ea7f25ba63f0eeee3100
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-12-20 17:28:22 +0800

    .

--- tf/007_lr_decay.py
+++ tf/007_lr_decay.py
@@ -6,7 +6,12 @@ decay_rate = 0.96
 global_steps = 1000
 decay_steps = 100
 
+# global_step refer to the number of batches seen by the graph. Everytime a batch is provided, the weights are updated in the direction that minimizes the loss. global_step just keeps track of the number of batches seen so far. When it is passed in the minimize() argument list, the variable is increased by one. Have a look at optimizer.minimize().
+#
+# You can get the global_step value using tf.train.global_step().
+# The 0 is the initial value of the global step in this context.
 global_ = tf.Variable(tf.constant(0))
+# staircase : 如果为 True global_step/decay_step 向下取整
 c = tf.train.exponential_decay(learning_rate, global_, decay_steps, decay_rate, staircase=True)
 d = tf.train.exponential_decay(learning_rate, global_, decay_steps, decay_rate, staircase=False)
 
