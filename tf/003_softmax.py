c0027b4787803b778c40db63a5afe24c907d4ec7
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-12-28 17:18:36 +0800

    learn softmax

---
+++ tf/003_softmax.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+
+# NN output
+logits = tf.constant([[1., 2, 3.],
+                      [1., 2, 3.],
+                      [1., 2., 3.]])
+
+y = tf.nn.softmax(logits=logits)
+
+print(sess.run(logits))
+print(sess.run(y))
+
+# true label
+y_ = tf.constant([[0., 0., 1.],
+                  [0., 0., 1.],
+                  [0., 0., 1.]])
+
+# calculate cross entroyp in one pass
+# logits instead of y!!!
+cross_entropy = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=logits))
+print(sess.run(cross_entropy))
+
+# calculate cross entropy
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+print(sess.run(cross_entropy))
